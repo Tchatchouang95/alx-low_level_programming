67c48311c6933adec3d9dbfc34861d721ca23002
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-02 20:23:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - Entry point
  *
  *   *
@@ -11,6 +10,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
