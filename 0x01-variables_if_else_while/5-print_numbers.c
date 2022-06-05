381b84233f83586c3aae6a709641687d27f0a9f2
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-05 08:13:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int num;
+	
+	num = 0;
+	while (num < 10)
+	{
+		printf("%d ", num);
+		num++;
+	}
+	return (0);
+}	
