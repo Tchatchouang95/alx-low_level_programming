82ce0dcbcc739cdd23860d3d6e9d184fd4d6f008
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-06 10:24:53 -0700

    101-quote.c

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
+	write(2, str, 59);
+	return (1);
+}
