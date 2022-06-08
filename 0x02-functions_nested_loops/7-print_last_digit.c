97c13b896ca1a456724444e6bfaa7939f1ddf1d1
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-08 06:52:35 -0700

    adding conditional statement for negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,10 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
-	return (last_digitlast_digit);
+	if (n < 0)
+	{
+		last_digit = (-1) * last_digit;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
