0975695c878d37a40445e3d44c8930e600b4ccce
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-08 16:54:20 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,19 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-				printf("%c", ',');
-				printf("%c", ' ');
-			}
 			n++;
 		}
 	}
 	else
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d", n);
-			if (n != 98)
-			{
-
-				printf("%c", ',');
-				printf("%c", ' ');
-			}
 			n--;
 		}
 	}
+	printf("%d\n", n);
 }
