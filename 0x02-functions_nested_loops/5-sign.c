67fef3df930f096d3ba1037c7407ca65c9fbaba5
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-07 18:31:01 -0700

    added description for print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print sign - determines if a number is positive or negative
+ * print_sign - determines if a number is positive or negative
  * @n: placeholder for our number
  * Description: check
  * Return: Always 0
