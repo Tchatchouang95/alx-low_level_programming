badbe66f73bd24fb10def88bbea6d13810f00477
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-08 16:02:40 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,8 +16,8 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				_putchar((n / 10 + '0');
-				_putchar((n % 10 + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 				break;
 			}
 			n++;
@@ -60,9 +60,9 @@ void print_to_98(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar((n / 10) + '0')
-		_putchar((n % 10) + '0')
-		_putchar('\n')
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
 	}
 	else 
 	{
