b351e7e7333011b95e680080d5684d3ae8ebba4c
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-08 10:58:45 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		{
 			if (w == 0)
 			{
-				_putchar(0 + '0');
+				_putchar(48);
 				continue;
 			}
 			product = l * w;
@@ -23,8 +23,8 @@ void times_table(void)
 			_putchar(' ');
 			if (product >= 10)
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				_putchar(product / 10 + 48);
+				_putchar(product % 10 + 48);
 			}
 			else
 			{
