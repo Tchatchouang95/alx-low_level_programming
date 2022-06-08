4a7b83b02f8ac0f6a8bb926f0a083b4ed3cb16cb
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-07 17:51:58 -0700

    checking letters of the alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns 1 or 0 depending on whether it alpha or not
+ *
+ * @c: placeholder
+ * Description: tests alphabets
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c >= 'a' && c <= 'Z' && c <= 'z')
+}
