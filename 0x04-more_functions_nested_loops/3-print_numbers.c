68dbc9187a2ca4892d0f832b36570ced40845879
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-09 09:36:43 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,16 @@
 /**
  * print_numbers - displays numbers of a given range
  *
+ * Description: prints the nombers from 0 to 9
  * Return: Always (0)
  */
 void print_numbers(void)
 {
 	char i;
 
-	i = 0;
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
-		i++;
 	}
 	_putchar('\n');
 }
