ff562341ad50c1aedf7c57394cfcb0d529d4f525
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-09 09:07:35 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -6,12 +6,9 @@
  *
  * @a: parameter 1
  * @b: parameter 2
- * Return: Always 0
+ * Return: Always (0)
  */
 int mul(int a, int b)
 {
-	int = product;
-
-	product = a * b;
-	return (product);
+	return (a * b);
 }
