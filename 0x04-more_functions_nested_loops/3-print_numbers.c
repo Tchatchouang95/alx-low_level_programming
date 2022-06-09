79612f41047a6fdbbf580efd5877897fb210ff11
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-09 11:07:42 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - displays the range of numbers from 0 to 9;
  * Return: Always 0
  */
+
 void print_numbers(void)
 {
 	int x;
@@ -12,5 +13,6 @@ void print_numbers(void)
 	{
 		_putchar(x + '0');
 	}
+
 	_putchar('\n');
 }
