936ea2148e318171b59d9bc3f9e826c8b57fe619
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-09 09:03:46 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - stands for product
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int = product;
+
+	product = a * b;
+	return (product);
+}
