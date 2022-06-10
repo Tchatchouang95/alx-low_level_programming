b735e446d6ff3cd85d89035e982bdccfad40750c
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-10 13:56:08 -0700

    function that prints  right angle triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: number of rows to display
+ *
+ * Description: Prints a pattern
+ * Return: Always (0)
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
