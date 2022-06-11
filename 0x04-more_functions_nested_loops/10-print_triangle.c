5d6b5c745f66aca61fd22573c99fb790597aaa42
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-10 18:26:59 -0700

    triangle pattern print

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,23 +11,16 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	for (i = 1; i <= size; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 1; i <= size; i++)
+		for (j = i; j < size; j++)
 		{
-			for (j = i; j < size; j++)
-			{
-				_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
+			_putchar(' ');
 		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
 }
