28fadd898ffe5a654dd52d0f43eb475d84dd0e5a
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-14 17:33:59 -0700

    Function that prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - start printing from the middle of a string
+ * @str: imput pointer
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int i, mid;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 != 0)
+	{
+		mid = (i - 1) / 2;
+		while (str[mid] != '\0')
+		{
+			_putchar(str[mid]);
+			mid++;
+		}
+	}
+	else
+	{
+		mid = i / 2;
+		while(str[mid] != '\0')
+		{
+			-putchar(str[mid]);
+			mid++;
+		}
+	}
+	_putchar('\n');
+}
