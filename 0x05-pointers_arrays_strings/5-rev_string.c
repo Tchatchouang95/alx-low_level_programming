25cc34a165ca9e5bee61d6e7da372fef7512768b
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-14 18:34:41 -0700

    code debug

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,22 +7,16 @@
  */
 void rev_string(char *s)
 {
-	int i, start, end, temp;
+	int i;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	start = i;
-	i = 0;
-	end = start - 1;
-	while (i < end)
+	while (i != 0)
 	{
-		temp = s[i];
-		s[i] = s[end];
-		s[end] = temp;
-		i++;
-		end++;
+		_putchar(s[i]);
+		i--;
 	}
 }
