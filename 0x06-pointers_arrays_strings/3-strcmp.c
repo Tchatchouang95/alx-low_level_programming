96659de69219bbd2666805e51ca5a63718ae8503
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-16 16:28:08 -0700

    debug code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,21 @@
  * _strcmp - to compare two strings
  * @s1: first input
  * @s2: second input
+ * Description: the function compares two strings
  * Return: return int
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i, gap;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; ++i)
 	{
 		if (s1[i] > s2[i])
 		{
 			gap = s1[i] - s2[i];
 			return (gap);
 		}
-		else if (s1[i] < s2[i])
+		else if (s1[i] <= s2[i])
 		{
 			gap = s2[i] - s1[i];
 			return (gap);
