370efcda1397c68b1e07725d5bf04abba959b079
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-19 16:16:23 -0700

    function that encodes a stringusing rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @s: input string
+ * Return: Always pointer
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char k[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; k[j] != '\0'; j++)
+		{
+			if (s[i] == k[j])
+			{
+				s[i] = k[j + 26];
+			}
+		}
+	}
+	return (s);
+}
