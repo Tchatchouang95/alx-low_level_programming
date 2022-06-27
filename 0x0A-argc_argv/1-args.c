5bf2d63757c934b181183f1aade2a55fc2508df7
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-06-27 13:44:22 -0700

    printing the number of arguements passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguements passed to it
+ * @argc: counter
+ * @argv: string holder
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
