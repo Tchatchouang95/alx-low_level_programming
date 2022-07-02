6ccba206802d76b1f085e5992cbe7bd1892bf326
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-07-01 19:30:44 -0700

    function that concatinates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l1, l2, i;
 	char *p;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	l1 = _strlen(s1);
