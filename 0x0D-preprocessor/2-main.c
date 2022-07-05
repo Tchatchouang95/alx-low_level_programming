1f3035e85fabaafac0fb9de2e04610e318f86bc1
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-07-05 14:53:32 -0700

    prints the file name from which the program is compiled using __FILE__ predefined macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the file name
+ * Description: Prints the file from which this program was compiled
+ * Return: Always 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
