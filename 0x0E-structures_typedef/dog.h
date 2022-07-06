3849ef71f5ea22e7aafea1dec3c7ed7ffa5b0112
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-07-05 18:17:27 -0700

    Defining a struct that will hold a dogs information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef puppy
+#define puppy
+
+/**
+ * struct dog - Defines a new type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
