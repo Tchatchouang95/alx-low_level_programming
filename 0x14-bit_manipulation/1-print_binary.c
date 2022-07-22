e99558f27e1f39ab1426e0f97c68c9e1ebdcece6
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-07-21 19:11:04 -0700

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print_binary - print the binary representation of a number
+ * @n: integer holder
+ */
+void print_binary(unsigned long int n)
+{
+	int prtbin = ((n & 1) + '0');
+	
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar(prtbin);
+}
