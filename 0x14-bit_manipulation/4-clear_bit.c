2d57085ce99392bb75c765e4ea6d2aa6048b9f49
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-07-21 19:57:23 -0700

    Function that clears a bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: a string
  * @index: index of a bit
- * Return 1 if true and -1 if false
+ * Return: 1 if true and -1 if false
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
