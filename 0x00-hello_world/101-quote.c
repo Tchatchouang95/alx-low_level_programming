47ddb7d0f0308e8c7f7c9fbc4e75e5ded6068fd8
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-08 18:18:50 -0800

    Fix betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, string, 59);
 
-	return(1);
+	return (1);
 }
