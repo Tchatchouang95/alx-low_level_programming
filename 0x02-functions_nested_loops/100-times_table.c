5b0de4b733df4485cd45706ec3404f058a683fd6
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-13 18:55:48 -0800

    code fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,15 +23,15 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				product = i * j;
-				if (product >= 10)
-				{
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else
+				if (product < 10)
 				{
 					_putchar(' ');
 					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
 				}
 				_putchar(',');
 				_putchar(' ');
