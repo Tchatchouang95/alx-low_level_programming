9a7f15d33b22caa30a7abffa5ae7b94222fc3346
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-14 18:36:38 -0800

    code fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,13 +30,14 @@ void print_times_table(int n)
 				}
 				else if (product < 100)
 				{
+					_putchar(' ');
 					_putchar(product / 10 + '0');
 					_putchar(product % 10 + '0');
 				}
 				else if (product >= 100)
 				{	
 					_putchar(product / 100 + '0');
-					_putchar(product / 10 + '0');
+					_putchar(product % 100 + '0');
 					_putchar(product % 10 + '0');
 				}
 				else
