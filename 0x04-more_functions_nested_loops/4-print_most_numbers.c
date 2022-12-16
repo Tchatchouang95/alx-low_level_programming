860c055f6ae9240c24daa2279dc214b9bf0ba46c
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-15 16:24:37 -0800

    Code fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,8 +10,7 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 		{
@@ -21,7 +20,6 @@ void print_most_numbers(void)
 		{
 			_putchar(i + '0');
 		}
-		i++;
 	}
 	_putchar('\n');
 }
