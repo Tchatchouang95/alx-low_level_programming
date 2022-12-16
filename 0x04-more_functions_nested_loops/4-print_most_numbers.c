97c9d3ea2ff4a6c2a2fcfd2a00b7c179d0e3de08
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-15 16:18:26 -0800

    Code debug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,11 +13,12 @@ void print_most_numbers(void)
 	i = 0;
 	while (i < 10)
 	{
-		if (i == 2 || i== 4)
+		if (i == 2 || i == 4)
 		{
 			continue;
 		}
 		_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
