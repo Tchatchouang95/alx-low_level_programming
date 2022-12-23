ced7904833b88a08a893fb1a073766a5aeff5e16
Tchatchouang95 <cedrickdjopgang@gmail.com>
2022-12-22 17:31:55 -0800

    Function to reverse an Array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array of numbers
+ * @a: input array
+ * @n: length of the array
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, f, temp;
+
+	i = 0;
+	f = n - 1;
+	while (i < f)
+	{
+		temp = a[i];
+		a[i] = a[f];
+		a[f] = tmp;
+		i++;
+		f--;
+	}
+}
