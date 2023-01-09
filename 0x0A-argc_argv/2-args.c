b9f7fdfafe6ff2f2d4b54999b2ce21ff9cb9214e
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-01-08 22:12:32 -0800

    Program that prints all arguements it recieves

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * @argc: Counter
+ * @argv: String of arguements passed 
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
