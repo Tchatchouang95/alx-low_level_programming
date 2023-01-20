9a09ab712992129a0000cab9b81e9cd425a9a5d7
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-01-19 18:55:22 -0800

    Code fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	va_list strings;
-	va_start (strings, n);
+	va_start(strings, n);
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
