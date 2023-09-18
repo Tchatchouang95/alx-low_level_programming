93fa901e90885ca4728519c9cfa0ace68c34dfad
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-18 04:21:19 -0700

    Function prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _function_pointers
 #define _function_pointers
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
