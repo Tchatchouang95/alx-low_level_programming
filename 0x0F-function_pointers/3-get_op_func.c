4a1756ecc839681e12805b832c14c515ba945894
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-19 09:24:57 -0700

    Code fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,23 +4,22 @@
 #include <string.h>
 
 /**
- * get_op_func - Finds the operator entered by a user
+ * get_op_func - Finds the operator entered by a user lamda
  * @s: operator passed as arguement
  * Return: Pointer to a function that corresponds to an operator
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
-	{
-		{"+", op_add}, 
-		{"-", op_sub}, 
-		{"*", op_mul}, 
-		{"/", op_div}, 
-		{"%", op_mod}, 
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
-       	int i = 0;
-	
+	int i = 0;
+
 	while (i < 5)
 	{
 		if (!strcmp(ops[i].op, s))
