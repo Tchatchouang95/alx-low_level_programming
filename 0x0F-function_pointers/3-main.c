5eec3abfe906d7e87c59e6daec26eef35ef90cb5
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-19 09:04:32 -0700

    Code fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,20 +13,19 @@ int main(int argc, char *argv[])
 	int a, b;
 	int (*ptr_func)(int, int);
 
-	if (argc < 3)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	ptr_func = get_op_func(argv[2]);
 	if (!ptr_func)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	printf("%d\n", ptr_func(a, b));
 	return (1);
 }
-
