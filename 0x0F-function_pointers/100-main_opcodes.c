9dcfbcd0547583bb33bcb4f5403f7a00521fd0b9
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-19 10:59:23 -0700

    Program that prints the opcodes of its main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that displays its opcodes
+ * @argc: length of arguements passed to the terminal
+ * @argv: stores the arguements passed to the terminal
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *array;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+		printf("%02hhx", array[i]);
+	}
+	return (0);
+}
