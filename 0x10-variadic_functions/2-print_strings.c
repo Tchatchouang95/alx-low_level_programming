970afc002c36ad777cbbad400339e5faffb4630a
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-19 19:00:51 -0700

    Code fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(args, char *);
 		if (string == NULL)
 		{
-			printf("(nil)");
+			printf("nil");
 		}
 		printf("%s", string);
 		if (separator != NULL && i < n - 1)
