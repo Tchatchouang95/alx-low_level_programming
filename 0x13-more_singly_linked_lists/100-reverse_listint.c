a1346f6d9fe19761f6a24473912bb886d344d1e8
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-26 17:22:12 -0700

    Function that reverses a string

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses a linked list
+ * @head: pointer to the first node
+ * Return: pointer to structure
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
+
