e3e3070621e764128ad17339968c4fe5b4e8b638
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-09-27 06:15:33 -0700

    Function that frees a list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,76 @@
 #include "lists.h"
 
 /**
+ * loop_listint_count - Counts the number of nodes in a looped link list
+ * @head: pointer to the first node of the link list
+ * Return: Always
+ */
+size_t loop_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+	{
+		return (0);
+	}
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
+/**
  * free_listint_safe - frees a list
  * @h: Pointer to the first node
  * Return: Number of element which has been set free
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t length = 0;
+	size_t nodes, index;
 	listint_t *temp;
-	int difference;
 
-	if (h == NULL || *h == NULL)
+	if (nodes == 0)
 	{
-		return (0);
-	}
-	while (*h != NULL)
-	{
-		difference = *h - (*h)->next;
-		if (difference > 0)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
 			temp = (*h)->next;
 			free(*h);
 			*h = temp;
-			length++;
-			break;
 		}
 	}
-	*h = NULL;
-	return (length);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+		*h = NULL;
+	}
+	h = NULL;
+	return (nodes);
 }
