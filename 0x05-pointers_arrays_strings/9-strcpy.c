ff6dbf2116502f2bd964ab895e6cbb8cee48dd2a
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-11-17 08:27:00 -0800

    Function that copies the string pointed to by a source file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from a source to a destination
+ * @dest: destination string
+ * @src: source string
+ * Return: duplicated string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*src[] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
