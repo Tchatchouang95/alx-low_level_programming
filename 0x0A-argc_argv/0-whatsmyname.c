ed7e6e51bf3fd048a9642a7fbf49067fbc05347b
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-11-17 13:00:12 -0800

    code fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: array of arguements passed to the terminal
  * Return: Always 0
  */
-int main(int argc__attribute__((unused)) , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
