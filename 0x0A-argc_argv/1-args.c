2f3b0605691ed2cebac9761a84d8486144cb87e4
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-11-17 13:08:02 -0800

    Program that prints the number of arguements passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: Number of arguements passed to cmd line
+ * @argv: Array of arguements passed to cmd line
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
