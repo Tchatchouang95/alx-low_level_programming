386216f894fc2aa72c4ff6e927c8d238a279ecb2
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-11-21 15:29:14 -0800

    Function that duplicates a string like the built-in function strdup()

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_len - Finds the length of a string
+ * @str: string
+ * Return: sring length
+ */
+int str_len(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - Duplicates a string
+ * @str: string
+ * Return: Duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *dest;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = str_len(str);
+	dest = (char *)malloc(len * sizeof(char));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
