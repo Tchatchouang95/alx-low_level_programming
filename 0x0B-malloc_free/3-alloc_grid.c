9749a0e1e0b1c578cd1f525a96ca6d6ceba083d4
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-11-21 17:51:40 -0800

    Function allocates memory to create a 2d array od intergers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a two dimensional array of integers
+ * @width: number of rows
+ * @height: number of culumns
+ * Return: pointer to the 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int) * height);
+
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(grid[i]);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
