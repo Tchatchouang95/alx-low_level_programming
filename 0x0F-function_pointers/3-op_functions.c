59d13382ae4bbe1488f1cf74d852e525d772f5fc
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-12-02 15:09:32 -0800

    Adding different function operations file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * op_add - Function that returns the sum of two integers
+ * @a: first variable
+ * @b: second variable
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Function that returns the diff of two int
+ * @a: first var
+ * @b: second var
+ * Return: difference
+*/
+int op_sub(int a, int b)
+{
+       	return (a - b);
+}
+
+/**
+ * op_mul - Function that returns the product of two int
+ * @a: first var
+ * @b: second var
+ * Return: product
+*/
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - Function that returns the division of two int
+ * @a: first var
+ * @b: second var
+ * Return: quotient
+*/
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_mod - Function that returns the modulo of two int
+ * @a: first var
+ * @b: second var
+ * Return: modulo
+*/
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
