643e67ce93d7bca3a6e8d6987581e9f66d03e802
Tchatchouang95 <cedrickdjopgang@gmail.com>
2023-12-18 15:11:41 -0800

    Adding header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
 #ifndef file_h
 #define file_h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcnt1.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
